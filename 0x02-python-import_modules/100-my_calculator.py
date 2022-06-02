c0e7b37b6d4f1302261d22f0b19d6ee959a1ffb1
MarkCuban <83606182+tenmark86@users.noreply.github.com>
2022-06-02 12:56:40 +0300

    program that imports all functions from the file calculator_1.py and handles basic operations.

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,32 +1,26 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
-
-
-def main():
-    import sys
-    if len(sys.argv) != 4:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit(1)
-    else:
-        sign = ['+', '-', '*', '/']
-        signok = 0
-        for i in sign:
-            if sys.argv[2] == i:
-                signok = 1
-                break
-        if signok == 0:
-            print("Unknown operator. Available operators: +, -, * and /")
-            exit(1)
-        else:
-            a = sys.argv[1]
-            b = sys.argv[3]
-            if sys.argv[2] == sign[0]:
-                print("{:s} + {:s} = {:d}".format(a, b, add(int(a), int(b))))
-            elif sys.argv[2] == sign[1]:
-                print("{:s} - {:s} = {:d}".format(a, b, sub(int(a), int(b))))
-            elif sys.argv[2] == sign[2]:
-                print("{:s} * {:s} = {:d}".format(a, b, mul(int(a), int(b))))
-            elif sys.argv[2] == sign[3]:
-                print("{:s} / {:s} = {:d}".format(a, b, div(int(a), int(b))))
 if __name__ == "__main__":
-    main()
+    import sys
+
+    nargs = len(sys.argv) - 1
+    if nargs != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+
+    op = sys.argv[2]
+    if op != '+' and op != '-' and op != '*' and op != '/':
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+
+    from calculator_1 import add, sub, mul, div
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+
+    if op == '+':
+        print("{} + {} = {}".format(a, b, add(a, b)))
+    elif op == '-':
+        print("{} - {} = {}".format(a, b, sub(a, b)))
+    elif op == '*':
+        print("{} * {} = {}".format(a, b, mul(a, b)))
+    else:
+        print("{} / {} = {}".format(a, b, div(a, b)))
