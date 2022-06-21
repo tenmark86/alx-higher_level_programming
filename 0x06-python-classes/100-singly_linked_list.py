9d755e136e7b872ab4905929551ffade3bf20471
MarkCuban <83606182+tenmark86@users.noreply.github.com>
2022-06-21 13:35:32 +0300

    7. Singly linked list

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -1,58 +1,76 @@
 #!/usr/bin/python3
-"""
-a class Square that defines a square by: 4-square.py
-"""
+"""Define classes for a singly-linked list."""
 
 
-class Square:
-    """
-    Private instance attribute: size
-    property def size(self): to retrieve it
-    property setter def size(self, value): to set it
-    Instantiation with optional size: def __init__(self, size=0):
-    Public instance method: def area(self):
-    that returns the current square area
-    Public instance method: def my_print(self):
-    that prints in stdout the square with the character #:
-    if size is equal to 0, print an empty line
-    """
+class Node:
+    """Represent a node in a singly-linked list."""
 
-    def __init__(self, size=0):
-
-        self.__size = size
-
-        if type(self.__size) is not int:
-            raise TypeError('size must be an integer')
-
-        if self.__size < 0:
-            raise ValueError('size must be >= 0')
+    def __init__(self, data, next_node=None):
+        """Initialize a new Node.
+        Args:
+            data (int): The data of the new Node.
+            next_node (Node): The next node of the new Node.
+        """
+        self.data = data
+        self.next_node = next_node
 
     @property
-    def size(self):
-        return self.__size
+    def data(self):
+        """Get/set the data of the Node."""
+        return (self.__data)
 
-    @size.setter
-    def size(self, size):
+    @data.setter
+    def data(self, value):
+        if not isinstance(value, int):
+            raise TypeError("data must be an integer")
+        self.__data = value
 
-        self.__size = size
+    @property
+    def next_node(self):
+        """Get/set the next_node of the Node."""
+        return (self.__next_node)
 
-        if type(self.__size) is not int:
-            raise TypeError('size must be an integer')
+    @next_node.setter
+    def next_node(self, value):
+        if not isinstance(value, Node) and value is not None:
+            raise TypeError("next_node must be a Node object")
+        self.__next_node = value
 
-        if self.__size < 0:
-            raise ValueError('size must be >= 0')
 
-        def area(self):
-            """
-            Returns square area
-            """
-        return self.__size * self.__size
+class SinglyLinkedList:
+    """Represent a singly-linked list."""
 
-    def my_print(self):
-        if self.__size > 0:
-            for i in range(self.__size):
-                for j in range(self.__size):
-                    print("#", end="")
-                print("")
+    def __init__(self):
+        """Initialize a new SinglyLinkedList."""
+        self.__head = None
+
+    def sorted_insert(self, value):
+        """Insert a new Node to the SinglyLinkedList.
+        The node is inserted into the list at the correct
+        ordered numerical position.
+        Args:
+            value (Node): The new Node to insert.
+        """
+        new = Node(value)
+        if self.__head is None:
+            new.next_node = None
+            self.__head = new
+        elif self.__head.data > value:
+            new.next_node = self.__head
+            self.__head = new
         else:
-            print("")
+            tmp = self.__head
+            while (tmp.next_node is not None and
+                    tmp.next_node.data < value):
+                tmp = tmp.next_node
+            new.next_node = tmp.next_node
+            tmp.next_node = new
+
+    def __str__(self):
+        """Define the print() representation of a SinglyLinkedList."""
+        values = []
+        tmp = self.__head
+        while tmp is not None:
+            values.append(str(tmp.data))
+            tmp = tmp.next_node
+        return ('\n'.join(values))
