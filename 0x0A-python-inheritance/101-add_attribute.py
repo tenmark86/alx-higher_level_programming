f8295c8a05f131cc2fb38c656257662debbfdef5
tenmark86 <tenmark86@gmail.com>
2022-07-04 04:00:56 -0700

    13. Can I?

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+a function that adds a new attribute to an object if its possible
+"""
+
+
+def add_attribute(obj, att, value):
+    """add_attribure: add a new objt
+    raise typerror if attribute not added
+    """
+    if type(obj) is int:
+        raise TypeError("can`t add new attribute")
+
+    return att, value
