3e79cc15726a5de1214a9fffce862c824a72b8c8
tenmark86 <tenmark86@gmail.com>
2022-07-07 02:24:53 -0700

    2. First Rectangle

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,135 @@
+#!/usr/bin/python3
+"""
+Write the class Rectangle that inherits from Base
+"""
+
+from models.base import Base
+
+
+class Rectangle(Base):
+    """ subclase de Base"""
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """lados del rectangulo"""
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        """getter of width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """setter of width"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """getter of height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """setter of height"""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = value
+
+    @property
+    def x(self):
+        """getter of x"""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """setter of x"""
+        if type(value) is not int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = value
+
+    @property
+    def y(self):
+        """getter of y"""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """setter of y"""
+        if type(value) is not int:
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = value
+
+    def area(self):
+        """
+        Calculate area of rectangle
+        Return area
+        """
+        return self.__width * self.__height
+
+    def display(self):
+        """print the rectangle with #"""
+        for y in range(0, self.__y):
+            print()
+        for i in range(0, self.__height):
+            for x in range(0, self.__x):
+                print(" ", end="")
+            for j in range(0, self.__width):
+                print("#", end="")
+            print()
+
+    def __str__(self):
+        """Return a string que represente al rectangulo"""
+        _str = "[Rectangle] ({}) {}/{} - {}/{}".format(
+                self.id, self.__x, self.__y, self.__width, self.__height)
+        return _str
+
+    def update(self, *args, **kwargs):
+        """Args, id, width, height, x and y"""
+
+        if args is not None and len(args) != 0:
+            if len(args) >= 1:
+                if type(args[0]) != int and args[0] is not None:
+                    raise TypeError("id must be an integer")
+                self.id = args[0]
+            if len(args) > 1:
+                self.width = args[1]
+            if len(args) > 2:
+                self.height = args[2]
+            if len(args) > 3:
+                self.x = args[3]
+            if len(args) > 4:
+                self.y = args[4]
+        else:
+            for key, value in kwargs.items():
+                if key == "id":
+                    if type(value) != int and value is not None:
+                        raise TypeError("id must be an integer")
+                    self.id = value
+                if key == "width":
+                    self.width = value
+                if key == "height":
+                    self.height = value
+                if key == "x":
+                    self.x = value
+                if key == "y":
+                    self.y = value
+
+    def to_dictionary(self):
+        """Return dictionaty representation a rectangle"""
+        my_dict = {'x': self.__x, 'y': self.__y, 'id': self.id,
+                   'height': self.__height, 'width': self.__width}
+        return my_dict
