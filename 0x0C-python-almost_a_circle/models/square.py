58d29be3a47db540149b9b24e14d1cf7d4d74024
MarkCuban <83606182+tenmark86@users.noreply.github.com>
2022-07-12 11:49:48 +0300

    Update square.py

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,23 +1,26 @@
 #!/usr/bin/python3
-"""
-Define a square class from rectangle"""
-
+# square.py
+# jackson muendo <jackmwexh1225@gmail.com>
+"""Defines a square class."""
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """Representation a scuare"""
+    """Represent a square."""
 
     def __init__(self, size, x=0, y=0, id=None):
-        """
-        Initialize a scuare
-        Args: size, id, x, y
+        """Initialize a new Square.
+        Args:
+            size (int): The size of the new Square.
+            x (int): The x coordinate of the new Square.
+            y (int): The y coordinate of the new Square.
+            id (int): The identity of the new Square.
         """
         super().__init__(size, size, x, y, id)
 
     @property
     def size(self):
-        """get,setter of size to square"""
+        """Get/set the size of the Square."""
         return self.width
 
     @size.setter
@@ -26,8 +29,15 @@ class Square(Rectangle):
         self.height = value
 
     def update(self, *args, **kwargs):
-        """Check corrects attribures"""
-
+        """Update the Square.
+        Args:
+            *args (ints): New attribute values.
+                - 1st argument represents id attribute
+                - 2nd argument represents size attribute
+                - 3rd argument represents x attribute
+                - 4th argument represents y attribute
+            **kwargs (dict): New key/value pairs of attributes.
+        """
         if args and len(args) != 0:
             a = 0
             for arg in args:
@@ -45,18 +55,18 @@ class Square(Rectangle):
                 a += 1
 
         elif kwargs and len(kwargs) != 0:
-            for key, var in kwargs.items():
-                if key == "id":
-                    if var is None:
+            for k, v in kwargs.items():
+                if k == "id":
+                    if v is None:
                         self.__init__(self.size, self.x, self.y)
                     else:
-                        self.id = var
-                elif key == "size":
-                    self.size = var
-                elif key == "x":
-                    self.x = var
-                elif key == "y":
-                    self.y = var
+                        self.id = v
+                elif k == "size":
+                    self.size = v
+                elif k == "x":
+                    self.x = v
+                elif k == "y":
+                    self.y = v
 
     def to_dictionary(self):
         """Return the dictionary representation of the Square."""
