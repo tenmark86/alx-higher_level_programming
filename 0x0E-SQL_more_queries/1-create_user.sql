a48b8c1cbee023d9b1e192b7029bf996f1e38594
tenmark86 <tenmark86@gmail.com>
2022-08-10 03:10:40 -0700

    script that creates the MySQL server user user_0d_1

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
--- create the mysql server user "user_0d_1", should have all privileges
--- and password should be set ti user_0d_1_pw
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pw';
-GRANT ALL PRIVILEGES ON *.* to 'user_0d_1'@'localhost';
+-- Script that creates an user in MySQL server
+-- Query to create the user 'user_0d_1' in MySQL server
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
