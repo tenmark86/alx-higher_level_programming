374a7e97a5ec7cd8710922fcf67769fd7f0bee7d
MarkCuban <83606182+tenmark86@users.noreply.github.com>
2022-09-23 19:09:56 +0300

    task 4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """
-fetches https://intranet.hbtn.io/status
+Script that fetches https://alx-intranet.hbtn.io/status
 """
-if __name__ == '__main__':
-    import requests
-    r = requests.get('https://intranet.hbtn.io/status')
-    text = r.text
-    print("Body response:")
-    print("\t- type: {}".format(type(text)))
-    print("\t- content: {}".format(text))
+import requests
+
+
+if __name__ == "__main__":
+    req = requests.get('https://alx-intranet.hbtn.io/status')
+    print('Body response:')
+    print('\t- type: {}'.format(type(req.text)))
+    print('\t- content: {}'.format(req.text))
