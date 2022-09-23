7eac604edba031d9a150dc7ba761ed153aee2af5
MarkCuban <83606182+tenmark86@users.noreply.github.com>
2022-09-23 19:02:21 +0300

    script that fetches https://alx-intranet.hbtn.io/status

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 """
-Python script that fetches https://intranet.hbtn.io/status
+fetches https://intranet.hbtn.io/status
 """
-
-import requests
-
-if __name__ == "__main__":
-    resp = requests.get('https://intranet.hbtn.io/status')
-    if resp is not None:
-        print("Body response:")
-        print("\t- type: {}".format(type(resp.text)))
-        print("\t- content: {}".format(resp.text))
+if __name__ == '__main__':
+    import requests
+    r = requests.get('https://intranet.hbtn.io/status')
+    text = r.text
+    print("Body response:")
+    print("\t- type: {}".format(type(text)))
+    print("\t- content: {}".format(text))
