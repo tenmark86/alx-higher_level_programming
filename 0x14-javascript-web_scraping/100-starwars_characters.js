71fcf50b5617f9b6b9f56668800464c71090462a
tenmark86e <tenmark86@gmail.com>
2022-10-18 15:34:48 +0300

    script that prints all characters of a Star Wars movie

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,17 +1,21 @@
 #!/usr/bin/node
-const request = require('request');
-const url = 'http://swapi.co/api/films/' + process.argv[2];
+/*
+ * Write a script that prints all characters of a Star Wars movie:
+ * The first argument is the Movie ID - example: 3 = “Return of the Jedi”
+ * Display one character name by line
+ */
 
-request(url, function (err, response, body) {
-  if (err == null) {
-    const resp = JSON.parse(body);
-    const characters = resp.characters;
-    for (let i = 0; i < characters.length; i++) {
-      request(characters[i], function (err, response, body) {
-        if (err == null) {
-          console.log(JSON.parse(body).name);
-        }
-      });
+const axios = require('axios');
+const url = ('https://swapi-api.hbtn.io/api/films/');
+const film = process.argv[2];
+
+axios.get(url + film)
+  .then(function (response) {
+    for (const x in response.data.characters) {
+      const people = response.data.characters[x];
+      axios.get(people)
+        .then(function (response) {
+          console.log(response.data.name);
+        });
     }
-  }
-});
+  });
